9d476a8473358a5d3a36cf1e715d9627f4630d98
maskeen <maskeensingh@protonmail.com>
2026-09-26 16:52:28 +0530

    feat: remove logo icon from navbar for pure typographic brandmark

--- frontend/app/_components/ChatInputBox.jsx
+++ frontend/app/_components/ChatInputBox.jsx
@@ -538,13 +538,6 @@ export default function ArchitecturalDesk() {
       {/* Top Apple Navigation Header */}
       <header className="apple-nav">
         <div className="nav-brand" onClick={resetToCenter} title="Reset drafting table">
-          <img
-            src="/icon.png"
-            alt="ErBlogX Logo"
-            className="brand-logo"
-            width={24}
-            height={24}
-          />
           <div className="brand-title">
             <em>Er</em>BlogX
           </div>
